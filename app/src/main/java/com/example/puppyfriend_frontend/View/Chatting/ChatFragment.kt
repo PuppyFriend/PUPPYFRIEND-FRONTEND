18dc0c5ad96eb0e4d82ce8f0edf8b2b931f15845
KimHyeJoo-sswu <tsukiame33@gmail.com>
2023-08-02 22:27:59 +0900

    [ADD] 리사이클러뷰 생성, 스와이프 이벤트 작성(테스트X) #ram

--- app/src/main/java/com/example/puppyfriend_frontend/View/Chatting/ChatFragment.kt
+++ app/src/main/java/com/example/puppyfriend_frontend/View/Chatting/ChatFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
 import com.example.puppyfriend_frontend.R
+import com.example.puppyfriend_frontend.databinding.FragmentChatBinding
 
 class ChatFragment : Fragment() {
+    lateinit var viewBinding: FragmentChatBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        viewBinding = FragmentChatBinding.inflate(inflater, container, false)
+
+        val swipeHelper = SwipeHelper()
+        val itemTouchHelper = ItemTouchHelper(swipeHelper)
+        itemTouchHelper.attachToRecyclerView(viewBinding.recyclerviewChatProfile)
+
+        return viewBinding.root
     }
 
 }
