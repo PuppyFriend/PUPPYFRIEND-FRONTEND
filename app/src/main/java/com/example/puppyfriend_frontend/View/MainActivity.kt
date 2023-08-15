b8a534fae6b0d8af63e7574c1cb1335e83148b1c
kchaeeun <oceanball38@naver.com>
2023-08-15 22:15:04 +0900

    [DEL] MainActivity 수정한 것 삭제#hyesung

--- app/src/main/java/com/example/puppyfriend_frontend/View/MainActivity.kt
+++ app/src/main/java/com/example/puppyfriend_frontend/View/MainActivity.kt
@@ -1,24 +1,12 @@
 package com.example.puppyfriend_frontend.View
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.example.puppyfriend_frontend.R
-import com.example.puppyfriend_frontend.View.Sns.SnsFragment
-import com.example.puppyfriend_frontend.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // HomeFragment를 로드하여 fragmentContainer에 추가합니다.
-        val homeFragment = SnsFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, homeFragment)
-            .addToBackStack(null)
-            .commit()
+        setContentView(R.layout.activity_main)
     }
 }
